24474ff894fa3ba454c0d6eb3402202520ffeb8e
Philip Taylor <ptaylor@taylorworld.one>
2019-05-17 10:11:04 +0100

    moved room dataabse into into statsDB.kt.

---
+++ app/src/main/java/com/taylorworld/tw01/statDB.kt
@@ -0,0 +1,24 @@
+package com.taylorworld.tw01
+
+@Database(
+    entities = [(Stats::class)],
+    version = 1,
+    exportSchema = false
+)
+abstract class DDDatabase : RoomDatabase() {
+
+    abstract fun statDao(): StatDao
+
+    private var inst: DDDatabase? = null
+    private val instLock = Object()
+
+    fun getInstance(context: Context): DDDatabase = inst ?: synchronized(instLock) {
+        return inst ?: run {
+            inst = Room.databaseBuilder(
+                context,
+                DDDatabas   e::class.java, "CharecterStats"
+            ).build()
+            inst!!
+        }
+    }
+}
--- app/src/main/java/com/taylorworld/tw01/stats.kt
+++ app/src/main/java/com/taylorworld/tw01/stats.kt
@@ -27,26 +27,3 @@ interface StatDao {
 
     //@Abstract val statDao: StatDao{}
 }
-
-@Database(
-    entities = [(Stats::class)],
-    version = 1,
-    exportSchema = false
-)
-abstract class DDDatabase : RoomDatabase() {
-
-    abstract fun statDao(): StatDao
-
-    private var inst: DDDatabase? = null
-    private val instLock = Object()
-
-    fun getInstance(context: Context): DDDatabase = inst ?: synchronized(instLock) {
-        return inst ?: run {
-            inst = Room.databaseBuilder(
-                context,
-                DDDatabase::class.java, "CharecterStats"
-            ).build()
-            inst!!
-        }
-    }
-}
