5511f6226d27f846f485152ef1d72a96debcaff6
Philip Taylor <ptaylor@taylorworld.one>
2019-05-18 10:04:50 +0100

    added firebase to app.

--- app/src/main/java/com/taylorworld/tw01/statDB.kt
+++ app/src/main/java/com/taylorworld/tw01/statDB.kt
@@ -1,25 +1,23 @@
-package com.taylorworld.tw01
+//package com.taylorworld.tw01
 
-@Database(entities = [(Stats::class)], version = 1, exportSchema = false
-)
-abstract class DDDatabase : RoomDatabase() {
-    abstract fun statDao(): StatDao
-    }
-
+//@Database(
+//    entities = [(Stats::class)],
+//    version = 1,
+//    exportSchema = false
+//)
 //abstract class DDDatabase : RoomDatabase() {
-
 //    abstract fun statDao(): StatDao
 
 //    private var inst: DDDatabase? = null
 //    private val instLock = Object()
 
- //   fun getInstance(context: Context): DDDatabase = inst ?: synchronized(instLock) {
- //       return inst ?: run {
- //           inst = Room.databaseBuilder(
- //               context,
- //               DDDatabase::class.java, "CharecterStats"
- //           ).build()
- //           inst!!
-  //      }
-  //  }
+//    fun getInstance(context: Context): DDDatabase = inst ?: synchronized(instLock) {
+//        return inst ?: run {
+//            inst = Room.databaseBuilder(
+//                context,
+//                DDDatabase::class.java, "CharecterStats"
+//            ).build()
+//            inst!!
+//        }
+//    }
 //}
